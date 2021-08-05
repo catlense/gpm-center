2b5b7658795fa5ea6a6a5ecaacc45c930d27b800
Maxim Voronin <catlense@voronin.xyz>
2021-08-06 03:32:24 +0700

    header block

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ГПМ-Центр</title>
+
+    <link rel="stylesheet" href="/css/common.css">
+</head>
+<body>
+    <header>
+        <div class="container main_container">
+            <a href="/" class="logo"></a>
+            <div class="addresses">
+                <p><span class="iconify" data-inline="false" data-icon="bytesize:location" style="font-size: 25px;"></span> г. Москва, ул. Бутлерова, д 17 Б</p>
+                <p>info@gpm-centr.ru / sales@gpm-centr.ru</p>
+            </div>
+            <p class="number">8 800 101 64 23</p>
+            <button>Заказать звонок</button>
+        </div>
+    </header>
+
+    <script src="https://code.iconify.design/1/1.0.6/iconify.min.js"></script>
+</body>
+</html>
