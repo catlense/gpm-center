7d24ff648d2516e203e6aaf9dd355a4b50be38c6
Maxim Voronin <catlense@voronin.xyz>
2021-08-11 20:23:58 +0700

    create templates

---
+++ modules/all_products.php
@@ -0,0 +1,6 @@
+<?php
+
+$categories = [];
+$products = [];
+
+?>
---
+++ product.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ГПМ-Центр</title>
+
+    <link rel="stylesheet" href="/css/common.css">
+</head>
+<body>
+    
+    <?php include 'modules/header.php'; ?>
+    <div class="product_container">
+        <div class="left_images"></div>
+        <div class="texts">
+            <h1>Дизельный погрузчик FD50Z</h1>
+            <p class="description">
+                Питание - Дизельное топливо<br>
+                Грузоподъемность - <b>5 000 кг</b><br>
+                Bыcoтa подъема - <b>3 000 мм</b><br>
+                Номинальная мощность - <b>92 кВт</b><br>
+                <b>Автоматическая коробка передач</b>
+            </p>
+        </div>
+    </div>
+    <table class="params">
+        <tr>
+            <td><b>Параметры машины</b></td>
+            <td><b>FD50Z</b></td>
+        </tr>
+        <tr>
+            <td>Питание</td>
+            <td>Дизельное топливо</td>
+        </tr>
+        <tr>
+            <td>Hoминaльнaя гpyзoпoдъeмнocть кг	</td>
+            <td>5000</td>
+        </tr>
+    </table>
+    <?php include 'modules/footer.php'; ?>
+</body>
+</html>
